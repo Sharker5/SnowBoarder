81e32a4be8b3da7df2af92dff414cd1021b66fa6
Sharker5 <24.bodyn.griffin@ksd.org>
2023-10-12 13:36:38 -0700

    Snowboarder

---
+++ Snowboarder/Assets/Script/Finishline.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Finishline : MonoBehaviour
+{
+    [SerializeField] float loadDelay = 1f;
+    [SerializeField] ParticleSystem finishEffect;
+
+     void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.tag == "Player")
+        {
+            finishEffect.Play();
+            GetComponent<AudioSource>().Play();
+            Invoke("ReloadScene", 2f);
+           
+        }
+       
+    }
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
---
+++ Snowboarder/Assets/Script/PlayerController.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float baseSpeed = 20f;
+
+
+
+    Rigidbody2D rb2d;
+    SurfaceEffector2D surfaceEffector;
+    bool canMove = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector = FindObjectOfType<SurfaceEffector2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canMove)
+        {
+            RotatePlayer();
+            RespondToBoost();
+        }
+    }
+
+    public void DisableControls()
+    {
+        canMove = false;
+    }
+
+    void RespondToBoost()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            surfaceEffector.speed = boostSpeed;
+        }
+        else
+        {
+            surfaceEffector.speed = baseSpeed;
+        }
+        
+       
+    }
+
+    void RotatePlayer()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb2d.AddTorque(torqueAmount);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb2d.AddTorque(-torqueAmount);
+        }
+    }
+}
